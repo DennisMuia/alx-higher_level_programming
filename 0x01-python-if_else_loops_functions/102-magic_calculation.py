99692a03a6a667ac203f25757e8053567ea667e3
DennisMuia <dennismuia.dm@gmail.com>
2022-11-29 03:23:54 -0800

    0x01. Python - if/else, loops, functions

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def magic_calculation(a, b, c):
+    """Similar bytecode by H.S."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -2,4 +2,4 @@
 """Prints the ASCII alphabet, in lowercase, not followed by a new line"""
 
 for letter in range(97, 123):
-    print("{}".format(ch(letter)), end="")
+    print("{}".format(ch(letter)), end=" ")
