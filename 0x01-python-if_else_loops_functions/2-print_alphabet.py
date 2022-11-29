0b34933b47acb82ccc27499187786af624df27f9
DennisMuia <dennismuia.dm@gmail.com>
2022-11-29 03:25:14 -0800

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-"""Prints the ASCII alphabet, in lowercase, not followed by a new line"""
-
 for letter in range(97, 123):
-    print("{}".format(ch(letter)), end=" ")
+    print("{}".format(chr(letter)), end=" ")
